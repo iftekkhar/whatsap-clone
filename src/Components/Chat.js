58d1bb61d892cd5a90a82832436929cae37e33d2
Iftekhar Anam <iftekhar_anam@hotmail.com>
2020-12-01 23:39:15 +0600

    Update Chat.js

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -4,58 +4,24 @@ import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
 import MicIcon from '@material-ui/icons/Mic';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import { AttachFile } from '@material-ui/icons';
-import React, { useContext, useEffect, useState } from 'react';
+import React from 'react';
 import './Chat.css';
-import { useParams } from 'react-router-dom';
-import db, { serverTime } from '../Assets/firebase';
-import { UserContext } from '../App';
-import firebase from 'firebase';
 
 
 
 function Chat() {
-    //LoggedIn User State
-    const [loggedInUser] = useContext(UserContext);
-    const [input, setInput] = useState('');
-    const { roomID } = useParams();
-    const [roomName, setRoomName] = useState('');
-    const [messages, setMessages] = useState([]);
-    useEffect(() => {
-        if (roomID) {
-            db.collection('rooms')
-                .doc(roomID)
-                .onSnapshot(snapshot => (
-                    setRoomName(snapshot.data().name)
-                ));
-            db.collection('rooms').doc(roomID)
-                .collection('messages')
-                .orderBy('timestamp', 'asc')
-                .onSnapshot(snapshot => setMessages(snapshot.docs.map(doc => doc.data()))
-                );
-        }
-    }, [roomID]);
+
 
     const sendMessage = (e) => {
-        e.preventDefault();
-        db.collection('rooms')
-            .doc(roomID)
-            .collection('messages')
-            .add({
-                message: input,
-                name: loggedInUser.name,
-                email: loggedInUser.email,
-                timestamp: firebase.firestore.FieldValue.serverTimestamp(),
 
-            });
-        setInput('');
     };
     return (
         <div className="chat">
             <div className="chat-header">
-                <Avatar src={`https://avatars.dicebear.com/api/identicon/${roomID}.svg`} />
+                <Avatar src={`https://avatars.dicebear.com/api/identicon/123.svg`} />
                 <div className="chat-header-info">
                     <h3>{roomName}</h3>
-                    <p>Last Seen {new Date(messages[messages.length - 1]?.timestamp?.toDate()).toUTCString()}</p>
+                    <p>Last Seen ...</p>
                 </div>
                 <div className="chat-header-right">
                     <IconButton>
@@ -71,10 +37,10 @@ function Chat() {
             </div>
             <div className="chat-body">
                 {messages.map(message => (
-                    <p className={`chat-message ${message.email === loggedInUser.email && "chat-reciever"}`}>
-                        <span className="chat-name">{message.name}</span>
-                        {message.message}
-                        <span className="chat-timestamp">{new Date(message.timestamp?.toDate()).toUTCString()}</span>
+                    <p className={`chat-message "chat-reciever"}>
+                        <span className="chat-name">name</span>
+                        message
+                        <span className="chat-timestamp">time</span>
                     </p>
                 ))}
 
@@ -82,7 +48,7 @@ function Chat() {
             <div className="chat-footer">
                 <InsertEmoticonIcon />
                 <form onSubmit={sendMessage}>
-                    <input value={input} onChange={e => setInput(e.target.value)} type="text" />
+                    <input value={} onChange={'do somthing;} type="text" />
                 </form>
                 <MicIcon />
             </div>
